f1933e635fff8dc3e6d5304256003ec3d17f6106
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2024-10-28 15:13:25 +0530

    minor changes

--- chart.js
+++ chart.js
@@ -1,11 +1,16 @@
 const data = JSON.parse(sessionStorage.getItem("forecastData")).list;
 
+function getDayName(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", { weekday: "long" });
+}
+
 if (data) {
   (async function () {
     new Chart(document.getElementById("myChart"), {
       type: "line",
       data: {
-        labels: data.map((row) => row.dt_txt),
+        labels: data.map((row) => getDayName(row.dt_txt.slice(0, 10))),
         datasets: [
           {
             label: "Temp ",
